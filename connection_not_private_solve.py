0011abb704caff657a01c3aeab55e93b73b9939e
Avinash015 <120790516+Avinash015@users.noreply.github.com>
2024-08-08 00:46:13 +0530

    Add files via upload

---
+++ connection_not_private_solve.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+# Path to your WebDriver
+driver_path = 'C:\\Users\\jadha\\OneDrive\\Desktop\\selenium\\chromedriver.exe'
+
+# Set Chrome options
+
+chrome_options = Options()
+chrome_options.add_argument('--allow-running-insecure-content')
+chrome_options.add_argument('--ignore-certificate-errors')
+
+# Initialize WebDriver with service and options
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.implicitly_wait(10)
+
+# Open the URL
+driver.get("https://expired.badssl.com/")
+
+# Print the text of the h1 element
+print(driver.find_element(By.TAG_NAME, 'h1').text)
+
+# Close the browser after a short delay
+time.sleep(3)
+driver.quit()
